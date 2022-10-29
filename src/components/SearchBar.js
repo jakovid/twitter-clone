bb6f17f33933a003cc850c599a7ce90d7a704032
Jacob Dowdle <jacobk.dowdle@gmail.com>
2022-10-29 21:41:35 +0800

    created initial files

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SearchBar(){
+    return(
+        <div id='searchBarContainer'>
+            <div>Search Icon</div>
+            <input type='text' placeholder='Search Twitter'/>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RouteSwitch from './RouteSwitch';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <RouteSwitch />
+);
