6864558ae5ecddc4f7069ce4b014bd9be7f7eb6e
Jacob Dowdle <jacobk.dowdle@gmail.com>
2022-10-30 11:45:45 +0800

    updated navigation bar

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function NavigationBar(){
+    return(
+        <div>
+            <div>Logo</div>
+            <div>
+                <img src={require('../assets/images/Hashtag.png')} />  
+                <div>Expolore</div>
+            </div>
+            <div>
+                <img src={require('../assets/images/Settings.png')} />
+                <div>Settings</div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import NavigationBar from "../components/NavigationBar";
 import SearchBar from "../components/SearchBar";
 
 export default function Home(){
     return(
         <div>
+            <NavigationBar />
             <SearchBar />
         </div>
     )
